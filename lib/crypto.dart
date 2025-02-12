25ef895a6a4bbf74276d97e8b09397a51d48644d
devfemibadmus <devfemibadmus@gmail.com>
2025-02-12 10:35:07 +0100

    ⚡ details o ⚡ verlay now 📚

--- lib/crypto.dart
+++ lib/crypto.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CryptoPage extends StatefulWidget {
   const CryptoPage({super.key});
@@ -29,6 +31,23 @@ class CryptoPageState extends State<CryptoPage>
     }
   }
 
+  Future<void> saveCoinsToCache(List<dynamic> coins) async {
+    final prefs = await SharedPreferences.getInstance();
+    final coinsJson = json.encode(coins);
+    await prefs.setString('cachedCoins', coinsJson);
+  }
+
+  Future<List<dynamic>> loadCoinsFromCache() async {
+    final prefs = await SharedPreferences.getInstance();
+    final coinsJson = prefs.getString('cachedCoins');
+    if (coinsJson != null) {
+      final List<dynamic> cachedCoins = json.decode(coinsJson);
+      return cachedCoins;
+    } else {
+      return [];
+    }
+  }
+
   void startAutoRefresh() {
     _timer = Timer.periodic(Duration(seconds: 2), (_) async {
       if (mounted && _isActive) {
@@ -51,11 +70,21 @@ class CryptoPageState extends State<CryptoPage>
 
   @override
   void initState() {
+    print("reinit");
     super.initState();
-    fetchCoins().then((coins) {
-      setState(() {
-        _cachedCoins = coins;
-      });
+    loadCoinsFromCache().then((coins) {
+      if (coins.isNotEmpty) {
+        setState(() {
+          _cachedCoins = coins;
+        });
+      } else {
+        fetchCoins().then((coins) {
+          setState(() {
+            _cachedCoins = coins;
+          });
+          saveCoinsToCache(coins);
+        });
+      }
     });
     startAutoRefresh();
   }
@@ -84,55 +113,61 @@ class CryptoPageState extends State<CryptoPage>
           setState(() {
             _cachedCoins = coins;
           });
+          saveCoinsToCache(coins);
         },
         child: ListView.builder(
+          addAutomaticKeepAlives: true,
           physics: BouncingScrollPhysics(),
           itemCount: _cachedCoins.length,
           itemBuilder: (context, index) {
             final coin = _cachedCoins[index];
-            final imageUrl =
-                'https://raw.githubusercontent.com/devfemibadmus/rate2ng/refs/heads/main/assets/${coin["nameid"]}.png';
-            return Card(
-              child: ListTile(
-                leading: Image.network(
-                  imageUrl,
-                  width: 40,
-                  height: 40,
-                  errorBuilder: (_, __, ___) => Icon(Icons.error),
-                ),
-                title: Row(
-                  children: [
-                    Text('${coin['symbol']}'),
-                  ],
-                ),
-                subtitle: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text('${coin['name']}'),
-                  ],
-                ),
-                trailing: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      '\$${double.parse(coin["price_usd"]).toStringAsFixed(2).replaceAll(RegExp(r'\B(?=(\d{3})+(?!\d))'), ',')}',
-                      style:
-                          TextStyle(fontWeight: FontWeight.w800, fontSize: 13),
-                    ),
-                    Text(
-                      '${(double.tryParse(coin["percent_change_24h"]) ?? 0) < 0 ? '' : '+'}${coin["percent_change_24h"]}%',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w800,
-                        fontSize: 10,
-                        color:
-                            (double.tryParse(coin["percent_change_24h"]) ?? 0) <
-                                    0
-                                ? const Color.fromARGB(255, 160, 11, 0)
-                                : const Color.fromARGB(255, 0, 121, 4),
+            return InkWell(
+              onTap: () => showCoinOverlay(context, coin),
+              child: Card(
+                child: ListTile(
+                  leading: CachedNetworkImage(
+                    cacheKey: 'coin_image_${coin["nameid"]}',
+                    imageUrl:
+                        'https://raw.githubusercontent.com/devfemibadmus/rate2ng-api/refs/heads/main/assets/${coin["nameid"] ?? 'default'}.png',
+                    width: 40,
+                    height: 40,
+                    fit: BoxFit.cover,
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                  ),
+                  title: Row(
+                    children: [
+                      Text('${coin['symbol']}'),
+                    ],
+                  ),
+                  subtitle: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('${coin['name']}'),
+                    ],
+                  ),
+                  trailing: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        '\$${double.parse(coin["price_usd"]).toStringAsFixed(2).replaceAll(RegExp(r'\B(?=(\d{3})+(?!\d))'), ',')}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w800, fontSize: 13),
                       ),
-                    ),
-                  ],
+                      Text(
+                        '${(double.tryParse(coin["percent_change_24h"]) ?? 0) < 0 ? '' : '+'}${coin["percent_change_24h"]}%',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 10,
+                          color: (double.tryParse(coin["percent_change_24h"]) ??
+                                      0) <
+                                  0
+                              ? const Color.fromARGB(255, 160, 11, 0)
+                              : const Color.fromARGB(255, 0, 121, 4),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             );
@@ -141,4 +176,168 @@ class CryptoPageState extends State<CryptoPage>
       ),
     );
   }
+}
+
+class CoinDetailsOverlay extends StatelessWidget {
+  final Map<String, dynamic> coin;
+  final VoidCallback onClose;
+
+  const CoinDetailsOverlay(
+      {super.key, required this.coin, required this.onClose});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.black54,
+      child: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.9,
+          height: MediaQuery.of(context).size.height * 0.8,
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 226, 226, 226),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Stack(
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: CachedNetworkImage(
+                        cacheKey: 'coin_image_${coin["nameid"]}',
+                        imageUrl:
+                            'https://raw.githubusercontent.com/devfemibadmus/rate2ng-api/refs/heads/main/assets/${coin["nameid"] ?? 'default'}.png',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    SizedBox(height: 8),
+                    _buildDetailRow('Rank', '#${coin['rank'] ?? ''}'),
+                    _buildDetailRow('Current Price',
+                        '\$${double.tryParse(coin["price_usd"] ?? '0')?.toStringAsFixed(2).replaceAll(RegExp(r'\B(?=(\d{3})+(?!\d))'), ',') ?? ''}'),
+                    _buildDetailRow('Market Cap',
+                        '\$${double.tryParse(coin["market_cap_usd"] ?? '0')?.toStringAsFixed(2).replaceAll(RegExp(r'\B(?=(\d{3})+(?!\d))'), ',') ?? ''}'),
+                    _buildDetailRow('Circulating Supply',
+                        '\$${double.tryParse(coin["csupply"] ?? '0')?.toStringAsFixed(2).replaceAll(RegExp(r'\B(?=(\d{3})+(?!\d))'), ',') ?? ''} ${coin['symbol'] ?? ''}'),
+                    Divider(),
+                    SizedBox(height: 8),
+                    Text(
+                      '24-Hour Performance',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    SizedBox(height: 8),
+                    _buildDetailRow(
+                      'Price Change (1h)',
+                      '${(double.tryParse(coin["percent_change_1h"] ?? '0') ?? 0) < 0 ? '' : '+'}${coin["percent_change_1h"] ?? ''}%',
+                      style: TextStyle(
+                        color: (double.tryParse(
+                                        coin["percent_change_1h"] ?? '0') ??
+                                    0) <
+                                0
+                            ? const Color.fromARGB(255, 160, 11, 0)
+                            : const Color.fromARGB(255, 0, 121, 4),
+                      ),
+                    ),
+                    _buildDetailRow(
+                      'Price Change (24h)',
+                      '${(double.tryParse(coin["percent_change_24h"] ?? '0') ?? 0) < 0 ? '' : '+'}${coin["percent_change_24h"] ?? ''}%',
+                      style: TextStyle(
+                        color: (double.tryParse(
+                                        coin["percent_change_24h"] ?? '0') ??
+                                    0) <
+                                0
+                            ? const Color.fromARGB(255, 160, 11, 0)
+                            : const Color.fromARGB(255, 0, 121, 4),
+                      ),
+                    ),
+                    _buildDetailRow(
+                      'Price Change (7d)',
+                      '${(double.tryParse(coin["percent_change_7d"] ?? '0') ?? 0) < 0 ? '' : '+'}${coin["percent_change_7d"] ?? ''}%',
+                      style: TextStyle(
+                        color: (double.tryParse(
+                                        coin["percent_change_7d"] ?? '0') ??
+                                    0) <
+                                0
+                            ? const Color.fromARGB(255, 160, 11, 0)
+                            : const Color.fromARGB(255, 0, 121, 4),
+                      ),
+                    ),
+                    Divider(),
+                    SizedBox(height: 8),
+                    Text(
+                      'Supply Details',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    SizedBox(height: 8),
+                    _buildDetailRow('Total Supply',
+                        '\$${double.tryParse(coin["tsupply"] ?? '0')?.toStringAsFixed(2).replaceAll(RegExp(r'\B(?=(\d{3})+(?!\d))'), ',') ?? ''} ${coin['symbol'] ?? ''}'),
+                    _buildDetailRow('Max Supply',
+                        '\$${double.tryParse(coin["msupply"] ?? '0')?.toStringAsFixed(2).replaceAll(RegExp(r'\B(?=(\d{3})+(?!\d))'), ',') ?? ''} ${coin['symbol'] ?? ''}'),
+                    Divider(),
+                    SizedBox(height: 8),
+                    Text(
+                      'Market Data',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    SizedBox(height: 8),
+                    _buildDetailRow('24h Trading Volume',
+                        '\$${double.tryParse(coin['volume24']?.toString() ?? '0')?.toStringAsFixed(2).replaceAll(RegExp(r'\B(?=(\d{3})+(?!\d))'), ',') ?? ''}'),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 0,
+                right: 0,
+                child: IconButton(
+                    icon: const Icon(Icons.close), onPressed: onClose),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDetailRow(String key, String value, {TextStyle? style}) {
+    return Row(
+      children: [
+        Text('$key: ',
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+        Expanded(
+          child: Text(value,
+              style: style ?? const TextStyle(fontSize: 16),
+              overflow: TextOverflow.ellipsis),
+        ),
+      ],
+    );
+  }
+}
+
+void showCoinOverlay(BuildContext context, Map<String, dynamic> coin) {
+  late OverlayEntry overlayEntry;
+  double opacity = 1.0;
+
+  overlayEntry = OverlayEntry(
+    builder: (context) => AnimatedOpacity(
+      opacity: opacity,
+      duration: Duration(milliseconds: 300),
+      child: CoinDetailsOverlay(
+        coin: coin,
+        onClose: () {
+          opacity = 0.0;
+          overlayEntry.markNeedsBuild();
+          Future.delayed(
+              Duration(milliseconds: 300), () => overlayEntry.remove());
+        },
+      ),
+    ),
+  );
+
+  Overlay.of(context).insert(overlayEntry);
 }
